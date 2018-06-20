34cd6bc9f0032ff55729e707960a02220ff36928
danitiemi <danitiemi.br@gmail.com>
2018-06-20 19:57:57 +0000

    creating the tweets feed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// $('article.tweet').hover(
+//     function() {
+//         $('#btn').fadeIn('slow');
+//     },function() {
+//         $('#btn').fadeOut('slow');
+//     }
+// );
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,21 +3,16 @@ $(document).ready(function() {
   //console.log("the DOM is ready for jQuery")
 // Set a variable for the max number of characters:
   var maxLength = 140;
-  // var counter = $(this)$('.counter');
+  var counter = $(".counter").parent("form");
   // Use the keyup event on the textarea, which fires each time a key is pressed when the textarea is in focus. Ideal:
+  $('textarea').on('keyup', function ( event ) {
 
-  $('textarea').keyup(function ( event ) {
-    // Check the value and get the length of it, store it in a variable:
-    var length = $(this).val().length;
-    // // Take that value away from your max width:
-    var length = maxLength-length;
-    // //  Put the result somewhere on the page:
-    $('.counter').text(length);
-  // });
-    if (length <= maxLength){
+    var charactersRemaining = maxLength - $(this).val().length;
+    $('.counter').text(charactersRemaining);
+    if (charactersRemaining < 0) {
       $('.counter').css('color', 'red');
     } else {
-      $('.counter').css('background-color', 'black');
+      $('.counter').css('color', 'black');
     }
   });
 
