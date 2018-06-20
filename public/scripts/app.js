c3036a6c74cd37057784cc46f5abd6805a832916
danitiemi <danitiemi.br@gmail.com>
2018-06-20 23:25:40 +0000

    add functions to render tweets dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $('article.tweet').hover(
-//     function() {
-//         $('#btn').fadeIn('slow');
-//     },function() {
-//         $('#btn').fadeOut('slow');
-//     }
-// );
+
+// responsible for taking in an array of tweet objects and then appending each one to the #tweets-container.
+function renderTweets (tweets) {
+
+  // let data = [];
+  // loops through tweets calls createTweetElement for each tweet
+  for (tweet in tweets) {
+    let singleTweet = createTweetElement(tweet);
+  }
+  // takes return value and appends it to the tweets container
+  data = $('#tweetsContainer').append(singleTweet);
+
+  return data;
+}
+
+// function that takes in a tweet object and is responsible for returning
+// a tweet <article> element containing the entire HTML structure of the tweet
+function createTweetElement (tweet) {
+  // returns a tweet <article>
+  //var $tweet = $("<article>").addClass("tweet");
+  let username = tweet.user.name;
+  let image = tweet.user.avatars.small;
+  let nickname = tweet.user.handle;
+  let content = tweet.content.text;
+  let tweetDate = tweet.created_at;
+
+  return `<header>${image}<span>${username}</span><span>${nickname}</span></header><section><p>${content}</p></section><footer><span>${tweetDate}</span>`;
+}
+
+renderTweets(data);
+
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+// $('#tweetsContainer').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
